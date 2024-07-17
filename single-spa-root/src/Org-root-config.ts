7a4cdffa27118127b335f98660ff15017df65cac
PradeepNannapaneni <dispradeep@gmail.com>
2024-07-17 23:18:56 +1000

    Added Comments for Single SPA Application

--- single-spa-root/src/Org-root-config.ts
+++ single-spa-root/src/Org-root-config.ts
@@ -1,19 +1,20 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
-
-
+// Register the 'home' application
 registerApplication({
   name: 'home',
   app: () => System.import('home') as Promise<LifeCycles<any>>,
-  activeWhen: ['/home'],
+  activeWhen: ['/home'], // Activate the 'home' application when the URL matches '/home'
 });
 
+// Register the 'styleguide' application
 registerApplication({
   name: 'styleguide',
   app: () => System.import('styleguide') as Promise<LifeCycles<any>>,
-  activeWhen: ['/'],
+  activeWhen: ['/'], // Activate the 'styleguide' application when the URL matches '/'
 });
 
+// Start the single-spa framework
 start({
-  urlRerouteOnly: false,
+  urlRerouteOnly: false, // Allow full page reloads when navigating between applications
 });
